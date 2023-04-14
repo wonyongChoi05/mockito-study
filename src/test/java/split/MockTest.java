a97b6bfc1d475fcfacb61d14e8e94d5968958c4c
splitube <voicesplit@daum.net>
2023-04-14 13:34:58 +0900

    test: 불필요한 Stubbing 테스트 추가

--- src/test/java/split/MockTest.java
+++ src/test/java/split/MockTest.java
@@ -5,6 +5,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,12 +18,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 public class MockTest {
 
     @Mock
-    private MockFixture nameWithMockAnnotation;
+    private MockFixture mockFixture;
 
     @DisplayName("목 객체는 스터빙 없이는 정상 테스트가 불가하다.( @Mock 사용 )")
     @Test
     public void mockObjectCanNotTestWithoutStubbingMockAnnotation() {
-        final String result = nameWithMockAnnotation.getName();
+        final String result = mockFixture.getName();
         Assertions.assertThat(result).isNotEqualTo("name");
     }
 
@@ -38,9 +39,9 @@ public class MockTest {
     @Test
     public void testMockWithStubbingWithAnnotation() {
         // Mock 객체에 대한 기대 설정
-        when(nameWithMockAnnotation.getName()).thenReturn("stubbing");
+        when(mockFixture.getName()).thenReturn("stubbing");
         // 테스트 코드 실행
-        final String nameWithMockAnnotationNameResult = nameWithMockAnnotation.getName();
+        final String nameWithMockAnnotationNameResult = mockFixture.getName();
         // 결과 검증
         Assertions.assertThat("stubbing").isEqualTo(nameWithMockAnnotationNameResult);
     }
@@ -61,18 +62,39 @@ public class MockTest {
     @DisplayName("메서드 호출 횟수를 검증한다 ( 한번 )")
     @Test
     public void verifyMethodOnce() {
-        nameWithMockAnnotation.getName();
-        verify(nameWithMockAnnotation).getName();
+        mockFixture.getName();
+        verify(mockFixture).getName();
     }
 
     @DisplayName("메서드 호출 횟수를 검증한다 ( 여러번 )")
     @Test
     public void verifyMethodManyTimes() {
-        nameWithMockAnnotation.getName();
-        nameWithMockAnnotation.getName();
-        nameWithMockAnnotation.getName();
-        nameWithMockAnnotation.getName();
-        verify(nameWithMockAnnotation, times(4)).getName();
+        mockFixture.getName();
+        mockFixture.getName();
+        mockFixture.getName();
+        mockFixture.getName();
+        verify(mockFixture, times(4)).getName();
     }
+
+    @Mock
+    List<String> mockList;
+
+    @DisplayName("불 필요한 Stubbing 이 존재할 경우 테스트가 실패한다 ( 내용 참조 )")
+    @Test
+    public void whenMockitoAnnotationsUninitialized_thenNPEThrown() {
+        //아래의 코드의 주석을 풀고 실행할 경우 테스트가 실패한다
+//        Mockito.when(mockList.size()).thenReturn(1);
+    }
+
+    /*
+     --ErrorMessage--
+
+     org.mockito.exceptions.misusing.UnnecessaryStubbingException:
+     Unnecessary stubbings detected.
+     Clean & maintainable test code requires zero unnecessary code.
+     Following stubbings are unnecessary (click to navigate to relevant line of code):
+     1. -> at split.MockTest.whenMockitoAnnotationsUninitialized_thenNPEThrown(MockTest.java:85)
+     Please remove unnecessary stubbings or use 'lenient' strictness. More info: javadoc for UnnecessaryStubbingException class.
+    */
 }
 
